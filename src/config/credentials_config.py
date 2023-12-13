01a01ad70a0b37367e931711f4b856bede22c6de
Vasyl Nuil <vnuil@highedgesoft.com>
2023-12-13 16:56:34 +0200

    added lab9

---
+++ src/config/credentials_config.py
@@ -0,0 +1,7 @@
+"""
+This file contains all the credentials used in the project
+"""
+# used for service RapidApi for LinkedIn Api
+X_RAPID_API_KEY = "43387c4b10msh471630d7c1a9f98p1c5ce0jsnbc2f1ea59b37"
+# used for service RapidApi for LinkedIn Api
+X_RAPID_API_HOST = "fresh-linkedin-profile-data.p.rapidapi.com"
