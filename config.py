2cd073729612b1f2f47d91d555f559bab01c77a4
Vasyl Nuil <vnuil@highedgesoft.com>
2023-12-13 16:13:13 +0200

    added lab7

---
+++ config.py
@@ -0,0 +1,6 @@
+# credentials
+X_RapidAPI_Key="43387c4b10msh471630d7c1a9f98p1c5ce0jsnbc2f1ea59b37"
+X_RapidAPI_Host="fresh-linkedin-profile-data.p.rapidapi.com"
+# urls
+get_personal_profile="https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile"
+get_profiles_posts="https://fresh-linkedin-profile-data.p.rapidapi.com/get-profile-posts"
